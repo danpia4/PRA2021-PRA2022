489136255ced1e1debbbdeff20c826d80c7b4546
s464925 <danpia4@at.amu.edu.pl>
2021-10-04 18:11:58 +0200

    first commit

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,17 @@
 package introduction;
+import org.apache.log4j.Logger;
 
 public class HelloWorld {
+    final static Logger log = Logger.getLogger("HelloWorld.class");
 
     public static void main(String [ ] args) {
+
+
+
         System.out.println("Hello! Witaj!");
+
+        log.info("test");
+
     }
+
 }
